5811e740d9555efbc1de88b2302bfee86aa1a236
Matvei Popov <mpopov@trinity.edu>
2022-09-20 15:55:39 -0700

    Update sm_setup.py

--- sm_setup.py
+++ sm_setup.py
@@ -4,8 +4,9 @@ from sagemaker.pytorch import PyTorch
 from sagemaker.tuner import IntegerParameter, CategoricalParameter, ContinuousParameter, HyperparameterTuner
 import yaml
 
-sagemaker_role = "arn:aws:iam::104995299079:role/service-role/AmazonSageMaker-ExecutionRole-20220411T130726"
+sagemaker_role = "*MyRole*"
 
+# Training Data Upload to S3
 """
 def upload_training_data(session,base_job_name):
     traindata_s3_prefix = '{}/data/train'.format(base_job_name)
@@ -28,26 +29,11 @@ def sagemaker_estimator(sagemaker_role, code_entry, code_dir, instance_type, ins
 
 
 def sagemaker_training(sm_estimator,train_s3,training_job_name):
-    print('here1')
     sm_estimator.fit(train_s3, job_name=training_job_name, wait=False)
-    print('here2')
-
-def sagemaker_hyperparam_tuning(sm_estimator, train_s3, hyperparameter_ranges, metric_definitions, tuning_job_name, max_jobs, max_parallel_jobs):
-    objective_metric_name = 'validation:error'
-    objective_type = 'Minimize'
-    tuner = HyperparameterTuner(estimator=sm_estimator,
-                                objective_metric_name=objective_metric_name,
-                                hyperparameter_ranges=hyperparameter_ranges,
-                                metric_definitions=metric_definitions,
-                                max_jobs=max_jobs,
-                                max_parallel_jobs=max_parallel_jobs,
-                                objective_type=objective_type)
-
-    tuner.fit(train_s3, job_name=tuning_job_name, wait=False)
 
 
 if __name__ == '__main__':
-    train_s3 = "s3://sagemaker-us-east-1-104995299079/AITraining/"
+    train_s3 = "*location*"
     code_entry = 'train.py'
     code_dir = os.getcwd()
     instance_type = 'ml.g5.xlarge'
@@ -73,17 +59,3 @@ if __name__ == '__main__':
     with open("config.yaml", "w") as f:
         yaml.dump(list_doc, f)
     sagemaker_training(sm_estimator, train_s3, training_job_name)
-
-    """
-    # sagemaker tuning job
-    hyperparameter_ranges = {
-        'epochs': IntegerParameter(50, 200),
-        'learning_rate': ContinuousParameter(0.0001, 0.1, scaling_type="Logarithmic"),
-        'batch_size': IntegerParameter(32, 256),
-        'drop_rate': ContinuousParameter(0.0, 1.0)
-    }
-    tuning_job_name = "tf-mnist-tuning-{}".format(strftime("%d-%H-%M-%S", gmtime()))
-    max_jobs = 4
-    max_parallel_jobs = 2
-    #sagemaker_hyperparam_tuning(sm_estimator, train_s3, hyperparameter_ranges, metric_definitions, tuning_job_name, max_jobs, max_parallel_jobs)
-    """
